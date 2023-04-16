54779b8ce4700dd02f8488e245cb1352236e7161
Alberto Miranda <codealchemist@gmail.com>
2023-04-15 22:54:41 -0300

    Proper path

--- print-ascii-art.js
+++ print-ascii-art.js
@@ -1,6 +1,9 @@
 import fs from 'fs'
+import path from 'path'
+import * as url from 'url'
 import chalk from 'chalk'
 
-const file = './ascii-art.txt'
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+const file = path.join(__dirname, 'ascii-art.txt')
 const art = fs.readFileSync(file, 'utf8')
 console.log(chalk.white(art))
